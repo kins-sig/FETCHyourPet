6dd59e096acba0e00ac252c38d38d66b1625931c
kins-sigm <ks3355@uncw.edu>
2019-11-04 14:23:21 -0500

    Main screen buttons!

--- app/src/main/java/com/sigm/fetchyourpet/MainActivity.java
+++ app/src/main/java/com/sigm/fetchyourpet/MainActivity.java
@@ -3,6 +3,7 @@ package com.sigm.fetchyourpet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickSignIn(View view){}
+    public void onClickSignUp(View view){}
+    public void onClickMainBrowse(View view){}
 }
